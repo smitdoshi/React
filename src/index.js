bc7abf23943dd33aff012e000d76eed25cd5724e
SMIT N DOSHI <smitdoshi007@gmail.com>
2016-10-13 02:26:06 -0400

    Fixed all errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';   // Is used to create and manage our components
+import ReactDOM from 'react-dom'; // is used to actually interact with application
 
 // Create a new component. This component should produce
 // some HTMl
 
 const App = function(){
-  return <div> Hi </div>;
+  return <div> Hello World from Scratch </div>;
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
 
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
